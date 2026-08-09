dd72045335f21c2e4bd8930b1a48fa3103a750ad
yash.meena <yash.meena@LPT5761.newgen.co.in>
2026-08-09 20:54:06 +0530

    initial commit.

---
+++ DSA/src/Arrays/palindromeNumber.java
@@ -0,0 +1,45 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class palindromeNumber {
+	
+	 public static boolean isPalindrome(int x) {
+
+
+
+	        String temp=String.valueOf(x);
+	        String[] arr=temp.split("");
+
+	        if(arr.length==1){
+	            return true;
+	        }
+	        if(x<0){
+	            return false;
+	        }
+
+	        int count1=0;
+	        int count2=arr.length-1;
+
+	        while(count1<count2){
+	            if(arr[count1].equals(arr[count2])){
+	                count1++;
+	                count2--;
+	            }else{
+	                return false;
+	            }
+	        }
+
+	        return true;
+	        
+	    }
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Please Enter the Number ");
+		int n=sc.nextInt();
+	    boolean ans=	isPalindrome(n);
+	    System.out.println(ans);
+	}
+
+}
