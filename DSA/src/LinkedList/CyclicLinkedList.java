5e200cceff23db8f187c2d4b5d307d20c2d45b3b
yash.meena <yashm1520@gmail.com>
2026-09-14 21:44:09 +0530

    Initial Commit.

---
+++ DSA/src/LinkedList/CyclicLinkedList.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+public class CyclicLinkedList {
+	
+	class ListNode {
+		     int val;
+		     ListNode next;
+		     ListNode(int x) {
+		         val = x;
+		         next = null;
+		      }
+	
+	 public boolean hasCycle(ListNode head) {
+	        if(head==null){
+	            return false;
+	        }
+	        ListNode slow=head;
+	        ListNode fast=head;
+
+	        while(fast.next!=null && fast.next.next!=null){
+	            slow=slow.next;
+	            fast=fast.next.next;
+
+	            if(slow==fast){
+	                return true;
+	            }
+	           
+
+	        }
+	        return false;
+	    }
+
+
+
+	}
+}
